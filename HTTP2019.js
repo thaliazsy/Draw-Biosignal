f9802f468a8c5a27356082467c3eb409e3d43bc5
Thalia <thalia_noga@yahoo.com>
2023-05-18 16:42:07 +0800

    first commit

---
+++ HTTP2019.js
@@ -0,0 +1,50 @@
+﻿var ret;
+//取得網路上的資源
+function HTTPGetBinaryData(urlStr) {
+    var HttpObj = new XMLHttpRequest();   
+	HttpObj.responseType = "arraybuffer"; //取得或設定實際回應類型  
+    HttpObj.onreadystatechange = function () {
+        if (HttpObj.readyState === 4) {
+            ret =  HttpObj.response;
+            // alert(ret);
+               callBack();
+			//   callBack(ret);
+				 
+         //   alert("data retrieved");
+        }
+    }
+	HttpObj.open("GET", urlStr, true);
+    HttpObj.send();
+}
+//取得網路上的資源
+function HTTPGetData(urlStr) {
+    var HttpObj = new XMLHttpRequest();   
+    HttpObj.onreadystatechange = function () {
+        if (HttpObj.readyState === 4) {
+            ret = HttpObj.responseText;
+            //   alert(ret);
+                 callBack();
+         //   alert("data retrieved");
+        }
+    }
+	HttpObj.open("GET", urlStr, true);
+    HttpObj.send();
+}
+
+//上傳 dataStr 到網路上
+function HTTPPostData(urlStr, dataStr ) {
+    var HttpObj = new XMLHttpRequest();
+	HttpObj.open("POST", urlStr, true);
+    HttpObj.setRequestHeader("Content-type", "application/json+fhir");
+   // HttpObj.setRequestHeader("Content-type", "application/xml+fhir");
+    HttpObj.onreadystatechange = function () {
+        if (HttpObj.readyState === 4) {
+            ret = HttpObj.responseText;
+          
+             alert(ret);
+        }
+    }
+
+    HttpObj.send(dataStr);
+  //  getData
+}
---
+++ drawGrid.js
@@ -0,0 +1,32 @@
+﻿function drawGrid(ctx) {
+    // Draw V lines
+    
+    ctx.beginPath();
+    ctx.strokeStyle="#FF0000";
+    for(i=0;i<1800;i=i+36)
+    {ctx.moveTo(i,0);
+     ctx.lineTo(i,800);
+    }
+    
+    for(i=0;i<800; i=i+40)
+    {ctx.moveTo(0,i);
+     ctx.lineTo(1800,i);
+    }
+    ctx.stroke();
+    
+    ctx.beginPath();
+    ctx.lineWidth= 3;
+    for(i=-1;i<=800; i=i+200)
+    {
+    ctx.moveTo(0,i);
+    ctx.lineTo(1800,i);
+    }
+    for(i=-1;i<=1800; i=i+360)
+    {
+    ctx.moveTo(i,0);
+    ctx.lineTo(i,800);
+    }
+    
+    ctx.stroke();
+    }
+
